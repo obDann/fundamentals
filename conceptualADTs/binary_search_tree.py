a7ca71f2cd0fb7563e49821bcf7d0b3454cabb0c
Dann <dj.sioson@mail.utoronto.ca>
2018-08-27 13:17:47 -0400

    BST: provided an interface to a binary search tree

---
+++ conceptualADTs/binary_search_tree.py
@@ -0,0 +1,58 @@
+class BinaryNode():
+
+    def __init__(self, data, left=None, right=None):
+        '''
+        (BSTNode, int, BSTNode, BSTNode) -> None
+
+        Initializes a Binary Node
+        '''
+
+
+class BinarySearchTree():
+
+    def __init__(self):
+        '''
+        (BinaryTree) -> None
+
+        Initializes a Binary Search Tree where the value to the left of the
+        tree is less than the root, and the value to the right of the tree
+        is greater than the root
+        '''
+
+    def add(self, value):
+        '''
+        (BinaryTree, int) -> None
+
+        Adds a value to the tree
+        '''
+
+    def remove(self, value):
+        '''
+        (BinaryTree, int) -> None
+
+        Removes the passed value
+        '''
+
+    def preorder_traversal(self):
+        '''
+        (BinaryTree) -> String
+
+        Returns a string representation of a preorder traversal (VLR) in the
+        BinaryTree
+        '''
+
+    def inorder_traversal(self):
+        '''
+        (BinaryTree) -> String
+
+        Returns a string representation of an inorder traversal (LVR) in the
+        BinaryTree
+        '''
+
+    def postorder_traversal(self):
+        '''
+        (BinaryTree) -> String
+
+        Returns a string representation of a postorder traversal (LRV) in the
+        BinaryTree
+        '''
