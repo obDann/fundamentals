9ca47c1a6e9ff9710b84b814b017054338352fa8
Dann <dj.sioson@mail.utoronto.ca>
2018-08-29 15:44:02 -0400

    MinHeap: Added interface

---
+++ conceptualADTs/min_heap.py
@@ -0,0 +1,22 @@
+class MinHeap():
+
+    def __init__(self):
+        '''
+        (MinHeap) -> None
+
+        Initializes the heap
+        '''
+
+    def insert(self, new_val):
+        '''
+        (MinHeap, float) -> None
+
+        Inserts an object to the MinHeap
+        '''
+
+    def remove_min(self):
+        '''
+        (MinHeap) -> float
+
+        Removes the smallest value in the heap
+        '''
